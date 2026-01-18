5dc4d61fb14865f2192e389ffb6be91594d6b888
Daniel Sergent <108247878+DSergent1@users.noreply.github.com>
2026-01-18 14:46:49 -0500

    Modified class for JavaDoc branch

--- cen4082Mod1.java
+++ cen4082Mod1.java
@@ -1,5 +1,14 @@
+/**
+ * The class shows a Fibonacci number example
+ * and calculates in the main class using 10.
+ */
 public class cen4082Mod1 {
 
+    /**
+     * Calculation method by entering the nth term using recursion.
+     * @param n position in the Fibonacci sequence
+     * @return nth Fibonacci number
+     */
         public static int fibonacciExample(int n) {
 
             if (n <= 1) {
@@ -8,8 +17,12 @@ public class cen4082Mod1 {
             return fibonacciExample(n - 1) + fibonacciExample(n - 2);
         }
 
-        // Main method
-        public static void main(String[] args) {
+    /**
+     * This is the main method to run and show the output for the Fibonacci sequence.
+     * Uses 10 to print out the 10th term.
+     * @param args argument for main method
+     */
+    public static void main(String[] args) {
             int n = 10;
             int result = fibonacciExample(n);
 
